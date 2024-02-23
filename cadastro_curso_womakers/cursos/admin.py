3ba07470653a3fa1f4e450612d10fce8ea5414f4
laicts <laisvictoria.cts@gmail.com>
2024-02-23 12:09:21 -0300

    modelando o app cursos

--- cadastro_curso_womakers/cursos/admin.py
+++ cadastro_curso_womakers/cursos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from cursos.models import Curso
 
 # Register your models here.
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'nivel', 'carga_horaria']
+    search_fields = ['titulo', 'nivel']
+    list_filter = ['data_do_curso']
--- cadastro_curso_womakers/projecto_womakers/urls.py
+++ cadastro_curso_womakers/projecto_womakers/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from base.views import *
+from cursos.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', inicio),
-    path('cadastro/', cadastro)
+    path('cadastro/', cadastro),
+    path('curso/', include('cursos.urls', namespace='cursos'))
 ]
